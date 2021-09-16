312d1564704820381bbcd4573bbdad950e2c5beb
Antoine Stevan <stevan.antoine@gmail.com>
2021-09-16 10:47:32 +0200

    Question 25.

--- solutions/25.sh
+++ solutions/25.sh
@@ -2,4 +2,4 @@
 # What is the most recent commit in the ~/MLclass folder?
 # response: Correction on xgboost BE, added solutions in separate folder
 
-git log --oneline -n 1
+ssh ctf "cd ~/MLclass && git log --oneline -n 1"
