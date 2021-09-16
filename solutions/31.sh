37ead53036c89731ca268129c33325bd127365dc
Mariana Alves <mariana.de-andrade-dias-alves@student.isae-supaero.fr>
2021-09-16 10:48:21 +0200

    Solution to Q31

---
+++ solutions/31.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#What is the full version of the commit ID of the first commit of the ~/transformers repository?
+#Solution: 43badf217d1ccfaf486e2cbb1b3567226b5e95bf
+
+transformers$ git log | tail
